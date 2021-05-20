d86cd440d72c2a6250669813153b97181fa033f1
Unieveth <git@unieveth.com>
2021-05-20 17:30:24 -0500

    init

---
+++ routes/boxers.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let boxerData = fs.readFileSync('./model/boxers.json');
+  var siteBoxer = JSON.parse(boxerData);
+
+  //Assigning the parsed array of objects read-in from boxers.json to a variable called createdBoxers
+  var createdBoxers = siteBoxer;
+  res.render('boxer', {createdBoxers});
+
+  res.send('respond with a resource');
+});
+
+module.exports = router;
